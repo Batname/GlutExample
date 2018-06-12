849bad316fcb9f8f649c0b0a582da559e3604072
Batname <dadubinin@gmail.com>
2018-06-12 18:12:48 +0200

    add red debug point

--- GlutExample/Main.cpp
+++ GlutExample/Main.cpp
@@ -149,10 +149,10 @@ private:
 
 	float DebugPointVertices[12] =
 	{
-		0.5f,  0.5f, 0.0f,  // top right
-		0.5f, -0.5f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f   // top left 
+		1.0f,  1.0f, 0.0f,  // top right
+		1.0f, -1.0f, 0.0f,  // bottom right
+		-1.0f, -1.0f, 0.0f,  // bottom left
+		-1.0f,  1.0f, 0.0f   // top left 
 	};
 
 	unsigned int DebugPointIndices[6] =
